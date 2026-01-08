bb1501ad711eb8e6a95d90e09046f1a6f8c550e5
setdek123 <rodriguezalan785@gmail.com>
2026-01-08 03:06:27 -0300

    fix prisma client for vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as any;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({ log: ["error"] });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
 
-// Usa NODE_ENV para controlar cache global
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
